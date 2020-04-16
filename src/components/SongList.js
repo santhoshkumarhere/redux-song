5f4791f1c3dc57e1939c3e925b027e89160e1144
Kumar, Santhosh <santhoshk.kumar@pioneer.com>
2020-04-16 16:29:49 -0500

    redux end

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,10 +1,37 @@
 import React, { Component} from 'react';
 import { connect } from 'react-redux';
+import { selectSong } from '../actions';
 
 class SongList extends Component {
+
+    //semantic UI classes are used in the jsx
+    renderList() {
+        return this.props.songs.map((song) => {
+            return (
+                <div className="item" key={song.title}> 
+                    <div className = "right floated content">
+                        <button 
+                            className="ui button primary"
+                            onClick={ () =>this.props.selectSong(song) }>Select
+                        </button>
+                    </div>
+
+                    <div className="content">{song.title}</div>
+                </div>
+            );
+        });
+    }
+
     render () {
-        return <div>SongList</div>
+        return <div className="ui divided list">{this.renderList()}</div>
     }    
 }
 
-export default connect()(SongList);
+const mapStateToProps = (state) => {
+    return {songs: state.songs}; // it will be available as a 'songs' props inside SongList component
+}
+
+export default connect(
+    mapStateToProps, 
+    { selectSong } //action creator function
+    )(SongList); // it will get latest state changs to this component also wire with actions
